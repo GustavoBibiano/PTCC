53cc5ee089d57453cf544c11973e92d9bcc4879b
Matheus2091 <matds.pires@gmail.com>
2019-10-31 07:27:50 +0900

    Contas a pagar / Estoque

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,5 +1,20 @@
 // This is a JavaScript file
 
 $(document).ready(function(){
+           $('.nav-search').hide();
             $('.sidenav').sidenav();
+            $('.collapsible').collapsible();
+            $('.fixed-action-btn').floatingActionButton();
+            $('.modal').modal();
+
+            $('.btn-delete').click(function(){
+              $('#tr4').hide();
+            });
+            $('.btn-add').click(function(){
+              $('#tr4').show();
+            });
+
+            $('.btn-search').click(function(){
+              $('.nav-search').toggle();
+            });
         });
